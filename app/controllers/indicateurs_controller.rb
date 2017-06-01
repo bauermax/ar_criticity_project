9b8383540726d5ad689743a93a4de441399af10d
Maxime Bauer <=>
2017-06-01 19:03:13 +0200

    v1

--- app/controllers/indicateurs_controller.rb
+++ app/controllers/indicateurs_controller.rb
@@ -1,10 +1,16 @@
 class IndicateursController < ApplicationController
   before_action :set_machine
+  before_action :set_indicateur, :only => [:edit,:update]
   def index
   end
 
   def new
     @indicateur = @machine.indicateurs.new
+  end
+
+  def edit
+    puts params.inspect
+
   end
 
   def create
@@ -16,13 +22,21 @@ class IndicateursController < ApplicationController
     end
   end
 
+  def update
+    if @indicateur.update(indicateur_params)
+      redirect_to machine_path(@machine)
+    else
+      render :edit
+    end
+  end
+
   private
   def set_machine
     @machine = Machine.friendly.find(params[:machine_id])
   end
 
   def set_indicateur
-    @indicateur = Indicateur.find(params[:indicateur_id])
+    @indicateur = Indicateur.find(params[:id])
   end
 
   def indicateur_params
